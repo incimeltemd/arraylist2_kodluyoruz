4da308df833e98f5337852da12b2c8c1e15c0dcc
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-02-28 15:45:43 +0300

    ödev yapıldı

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+
+internal class Program
+{
+    private static List<int> Main(string[] args)
+    {
+        ArrayList liste = new ArrayList();
+        liste.Add("Ayşe");
+        liste.Add('d');
+        liste.Add(3);
+        liste.Add(true);
+
+        Console.WriteLine(liste[2]);
+
+        string[] renkler = {"kırmızı","beyaz","mavi"};
+        List<int> sayılar = new List<int> {3,4,5,2}
+
+        //add range dizi ekleme
+        liste.AddRange(renkler);
+        liste.AddRange(sayılar);
+
+        foreach(var item in liste)
+            Console.WriteLine(item);
+
+            //sort sıralama liste.sort
+            //liste.binarysearch , sıralı dizide aranan degern index ini verir
+
+        //reverse <----- ---->
+        liste.Reverse();
+        foreach(var item in liste)
+            Console.WriteLine(item);
+;    }
+}
